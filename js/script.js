2b429dc37d671dc3e875221c42cb696dcb59a3fa
Isaac <uniqucode@gmail.com>
2022-09-27 16:53:29 +0100

    Added functionality

--- js/script.js
+++ js/script.js
@@ -31,4 +31,11 @@ function sendNumberValue(number) {
   }
 }
 
-function addDecimal() {}
+function addDecimal() {
+   // If operator pressed, don't add decimal
+   if (awaitingNextValue) return;
+   // If no decimal, add one
+   if (!calculatorDisplay.textContent.includes('.')) {
+     calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+   }
+}
