36d1bab11b5ab36795bf3e51bcd6274e6c099229
Isaac <uniqucode@gmail.com>
2022-09-27 16:46:38 +0100

    Added calculating first & second value

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 const calculatorDisplay = document.querySelector('h1');
 const inputBtns = document.querySelectorAll('button');
 const clearBtn = document.getElementById('clear-btn');
+
+// Calculate first and second value depending on operator
+const calculate = {
+  '/': (firstNumber, secondNumber) => firstNumber / secondNumber,
+
+  '*': (firstNumber, secondNumber) => firstNumber * secondNumber,
+
+  '-': (firstNumber, secondNumber) => firstNumber - secondNumber,
+
+  '+': (firstNumber, secondNumber) => firstNumber + secondNumber,
+
+  '=': (firstNumber, secondNumber) => secondNumber,
+};
